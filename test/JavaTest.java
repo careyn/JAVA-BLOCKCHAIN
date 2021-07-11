20d0cb862e8b1b01893490ff211ba56756891bf6
careyn <carey.n@northeastern.edu>
2021-07-11 17:43:18 -0400

    Adding testing

---
+++ test/JavaTest.java
@@ -0,0 +1,19 @@
+package test;
+import junit.framework.*;
+import src.Block;
+
+public class JavaTest extends TestCase {
+   protected String[] genTransactions = {"example transaction 1", "example transaction 2"};
+   protected String[] twoTransactions = {"example transaction 3", "example transaction 4"};
+   
+   // assigning the values
+   protected void setUp(){
+   }
+
+   // test method to add two values
+   public void testAdd(){
+      Block genBlock = new Block(0, genTransactions);
+      Block blockTwo = new Block(0, twoTransactions);
+      assertTrue(genBlock != blockTwo);
+   }
+}
